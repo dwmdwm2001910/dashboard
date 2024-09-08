d1ad07ece96622dbdfc504acb2c80db8f0d1bce9
dong <dong@dongs-MacBook-Pro-2.local>
2024-09-07 22:38:03 -0400

    next dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next"
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
 const Customers = () => {
     return <p>Customer Page</p>
-}
+};
 
 export default Customers
